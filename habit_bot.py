ba3b4f34f17b17c8a604afe894f70ddd603f6069
DilmurodKhushboyev <telegram28282828@gmail.com>
2026-03-07 09:48:37 +0500

    Add files via upload

--- habit_bot.py
+++ habit_bot.py
@@ -6602,7 +6602,7 @@ try:
 
     @api_app.route("/api/groups/<int:uid>")
     def api_groups(uid):
-        all_groups = db["groups"].find({"members": str(uid)}) if db else []
+        all_groups = mongo_db["groups"].find({"members": str(uid)}) if mongo_db else []
         result = []
         for g in all_groups:
             members_raw = g.get("members", [])
